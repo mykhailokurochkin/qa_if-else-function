c1534255479a70ce5b31da380904a80d63498258
Mykhailo <kurochkinmy@gmail.com>
2025-11-09 11:02:07 +0100

    test: add tests for isElse function

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,31 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it('should not return anything', () => {
+    expect(
+      ifElse(
+        () => true,
+        () => {},
+        () => {}
+      )
+    ).toBeUndefined();
   });
 
-  // write tests here
+  it("should call 'first' if condition returns 'true'", () => {
+    const f = jest.fn();
+
+    ifElse(() => true, f, () => {});
+
+    expect(f).toBeCalled();
+  });
+
+  it("should call 'second' if condition returns 'false'", () => {
+    const f = jest.fn();
+
+    ifElse(() => false, () => {}, f);
+
+    expect(f).toBeCalled();
+  });
 });
